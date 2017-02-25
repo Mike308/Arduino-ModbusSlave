6693312e68335616d23ae3e6146a0c7f7bf15ff3
Majkel44 <m.werner9205@gmail.com>
2017-02-25 13:57:27 +0100

    - added communication via Modbus Protocol - added reading temperatures from ds18b20 sensors

--- ModbusSlave.ino
+++ ModbusSlave.ino
@@ -1,3 +1,14 @@
+#include <ModbusSerial.h>
+#include <Modbus.h>
+#include <DallasTemperature.h>
+#include <OneWire.h>
+
+
+#define ONE_WIRE_BUS 3
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+ModbusSerial mb;
 
 
 
@@ -5,6 +16,13 @@ void setup()
 {
 
   /* add setup code here */
+  mb.config(&Serial,19200,SERIAL_8N1);
+  mb.setSlaveId(10);
+  mb.addIreg(100);
+  mb.addIreg(101);
+  sensors.begin();
+  
+  
   
 
 }
@@ -13,5 +31,15 @@ void loop()
 {
 
   /* add main program code here */
+ 
+  mb.task();
+   sensors.requestTemperatures();
+  mb.Ireg(100, (int)sensors.getTempCByIndex(0));
+  mb.Ireg(101, (int)sensors.getTempCByIndex(1));
+  
+  
+  
+  
+  
 
 }
